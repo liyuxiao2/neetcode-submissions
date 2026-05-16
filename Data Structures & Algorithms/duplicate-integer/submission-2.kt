a8071648e5e433a10c47ba51d56b3dff3b15a383
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-05-15 22:23:03 -0400

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        val seen = HashSet<Int>()
+
+        for(num in nums){
+            if (num in seen) {
+                return true
+            }
+            seen.add(num)
+        }
+        return false
+    }
+}
