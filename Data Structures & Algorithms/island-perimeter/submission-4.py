ecd1d88a3c6a053f118ab5251fa4ca59a76e498f
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-05-23 12:03:14 -0400

    Add: island-perimeter - submission-4

---
+++ Data Structures & Algorithms/island-perimeter/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        max_perim = 0
+        row, col = len(grid), len(grid[0])
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        seen = set() 
+
+
+
+        def dfs(x, y):           
+            perim = 0
+            stack = [(x,y)]
+            seen.add((x,y))
+
+            while stack:
+                x, y = stack.pop()
+
+                for d in directions:
+                    dx, dy = d
+
+                    new_x, new_y = x + dx, y + dy
+
+                    if not (0 <= new_x < row and 0 <= new_y < col) or grid[new_x][new_y] == 0:
+                        perim += 1
+                    elif (new_x, new_y) not in seen and grid[new_x][new_y] == 1:
+                        stack.append((new_x, new_y))
+                        seen.add((new_x,new_y))
+
+            return perim
+
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 1:
+                    max_perim = max(max_perim, dfs(i, j))
+        return max_perim
+                    
