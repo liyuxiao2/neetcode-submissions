84e6bb4d8d2d1990f91196079c3b59f94ce8d4a6
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-06-03 20:42:40 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+
+        def dfs(i):
+            if i > len(nums) - 1:
+                return 0
+            
+            if i in cache:
+                print("hit index " + str(i) + " : " + str(cache[i]))
+                return cache[i]
+            
+            cache[i] = max(nums[i] + dfs(i+2), dfs(i+1))
+
+            return cache[i]
+
+        return dfs(0)
+
+        
