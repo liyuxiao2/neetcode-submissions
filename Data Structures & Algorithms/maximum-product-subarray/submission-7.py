4afc44ae12d0399926ff64fa1b8a358e0eaec9bf
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-06-06 14:41:49 -0400

    Add: maximum-product-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # at every index, we calculate the max product we can obtain
+        # itself, or multiplying against previous maxes
+
+        # [[2, 2], [8, 4], [-3, -24], 120]
+
+        
+        
+        global_max = nums[0]
+        c_min = nums[0]
+
+        dp = [nums[0]]
+
+        for i in range(1, len(nums)):
+            #start a new subarr, multiply by min or multiply by max
+            c_max = max(nums[i] * dp[i-1], nums[i] * c_min, nums[i])
+            c_min = min(nums[i] * dp[i-1], nums[i] * c_min, nums[i])
+            dp.append(c_max)
+
+            global_max = max(c_max, global_max)
+
+
+
+        
+        
+        
+        return global_max
+        
