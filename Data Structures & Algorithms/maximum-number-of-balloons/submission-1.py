e8ae8735b609d353cecf1a6b0e7971854f66b4c2
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-07-02 01:22:16 -0400

    Add: maximum-number-of-balloons - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        items = {}
+        count = 0
+        for i in text:
+            if i in "balloon":
+                items[i] = items.get(i, 0) + 1
+        
+        while True:
+            if items.get("b", 0)  >= 1 and items.get("a", 0) >= 1 and items.get("l", 0) >= 2 and items.get("o", 0) >= 2 and items.get("n", 0) >= 1:
+                items["b"] -= 1
+                items["a"] -= 1
+                items["l"] -= 2
+                items["o"] -= 2
+                items["n"] -= 1
+                count += 1
+            else:
+                return count
