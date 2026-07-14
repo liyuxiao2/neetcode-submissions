339fa1c2d70ba63124c563baa30f149816fb664f
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-07-14 10:54:21 -0400

    Add: word-pattern - submission-2

---
+++ Data Structures & Algorithms/word-pattern/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        d = {}
+        chars = set()
+        s = s.split(" ")
+
+        if len(s) != len(pattern):
+            return False
+
+        for i in range(len(s)):
+            if pattern[i] in d or s[i] in chars:
+                if (s[i] in chars and pattern[i] not in d) or not (d[pattern[i]] == s[i] and pattern[i] not in d.values()):
+                    return False
+            else:
+                chars.add(s[i])
+                d[pattern[i]] = s[i]
+            
+            print(d, chars)
+        
+        return True
