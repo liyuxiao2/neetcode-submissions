03919bf3973d24a5f92687ccda399f34c4f32f55
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-08-12 11:07:25 -0400

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        cache = {len(s): True}
+
+        def dfs(i):
+            if i in cache:
+                return cache[i]
+            
+            for w in wordDict:
+                if i + len(w) <= len(s) and s[i:i + len(w)] == w:
+                    if dfs(i + len(w)):
+                        cache[i] = True
+                        return True
+            #didnt match anything we get here
+            cache[i] = False
+            return False
+        
+        return dfs(0)
+                
