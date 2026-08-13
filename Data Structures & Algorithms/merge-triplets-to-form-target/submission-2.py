3a6576b3896adfc9c6d04d7237f3d876b91bf469
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-08-13 12:26:00 -0400

    Add: merge-triplets-to-form-target - submission-2

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        cur_t = [0] * 3
+
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+            
+            cur_t = [max(cur_t[0], t[0]), max(cur_t[1], t[1]), max(cur_t[2], t[2])]
+
+            if cur_t == target:
+                return True
+        
+        return False
