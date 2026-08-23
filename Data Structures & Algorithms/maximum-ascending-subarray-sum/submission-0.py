aae1e81697f09b04037cc2f0753631ae1e8ae8a1
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-08-23 01:26:56 -0400

    Add: maximum-ascending-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        max_s = nums[0]
+        cur_s = nums[0]
+
+        for i in range(1, len(nums)):
+            print(cur_s)
+            if nums[i] > nums[i-1]:
+                cur_s += nums[i]
+            else:
+                cur_s = nums[i]
+            
+            max_s = max(max_s, cur_s)
+
+        return max_s
