7c699e332fa94aa84297429eb6cbc40581f68795
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-08-25 15:20:27 -0400

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        maps = {}
+
+        for i in range(len(nums2)):
+            j = i + 1
+
+            while j < len(nums2) and nums2[i] > nums2[j]:
+                j += 1
+            
+            maps[nums2[i]] = nums2[j] if j < len(nums2) else -1
+
+        
+
+        for i in range(len(nums1)):
+            nums1[i] = maps[nums1[i]]
+            
+        return nums1
+
+                
