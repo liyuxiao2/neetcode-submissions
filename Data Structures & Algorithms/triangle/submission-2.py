880cb69109d52e5690e1fe7cd0ef21d2e50d8bd9
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-08-30 22:33:30 -0400

    Add: triangle - submission-2

---
+++ Data Structures & Algorithms/triangle/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        #choice to continue down j + 1, or just by j
+
+        #we break once we hit i == len(triangle) or j == len(triagnel[0])
+
+        row = len(triangle)
+
+        dp = {}
+
+        def dfs(i, j):
+            if i == row:
+                return 0
+            if (i , j) in dp:
+                return dp[(i, j)]
+            
+            res = min(triangle[i][j] + dfs(i + 1, j), triangle[i][j] + dfs(i + 1, j + 1))
+
+            dp[(i, j)] = res
+
+            return res
+        
+        
+        return dfs(0, 0)
+
