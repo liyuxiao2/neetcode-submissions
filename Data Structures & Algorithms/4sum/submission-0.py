f126c4760fcf16d19dcb7c374fcdbcd4d2acaa7f
Liyu Xiao <89355060+liyuxiao2@users.noreply.github.com>
2026-09-07 14:00:28 -0400

    Add: 4sum - submission-0

---
+++ Data Structures & Algorithms/4sum/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        nums.sort()
+
+        seen = set() # store tuples
+
+        #[-3, 0, 1, 2, 3, 3]
+
+        print(nums)
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                l, r = j+1, len(nums)-1
+
+                first, second = nums[i], nums[j]
+                while l < r:
+                    if nums[l] + nums[r] + first + second > target:
+                        r -=1
+                    elif nums[l] + nums[r] + first + second < target:
+                        l += 1
+                    else:
+                        if (first, second, nums[l], nums[r]) not in seen:
+                            res.append([first, second, nums[l], nums[r]])
+                            seen.add((first, second, nums[l], nums[r]))
+                        l += 1
+        return res
+
